435cf046dc671cbe272ad05407faf11f69029ad8
LiamTeixido <114273617+LiamTeixido@users.noreply.github.com>
2023-07-06 18:56:24 -0500

    Botón comprar y wsp

--- Assets/Scripts/ButtonWeb.cs
+++ Assets/Scripts/ButtonWeb.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class ButtonWeb : MonoBehaviour
 {
-    public string url = "https://instagram.com/pawcraftperu?igshid=YzcxN2Q2NzY0OA=="; 
+    public string url = "https://instagram.com/pawcraftperu?igshid=YzcxN2Q2NzY0OA==";
+    public string wp = "https://wa.me/51935560273";
 
     public void OpenExternalURL()
     {
         Application.OpenURL(url);
+    }
+
+    public void OpenWhatsapp()
+    {
+        Application.OpenURL(wp);
     }
 }
